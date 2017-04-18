de1354d7b2ffdafed23c233a70f55f22cbe31fb7
Roman Guryanov <spotirguryanov@gmail.com>
2017-04-18 21:39:58 +0300

    workaround for dotnet-cli-5993

---
+++ ConsoleApp1/ClassLibrary1/AsyncService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class AsyncService
+    {
+	    public async Task SomethingAsync()
+	    {
+#if NET40
+			await TaskEx.Delay(1000);  
+#else
+		    await Task.Delay(1000);
+#endif
+		}
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+	using System.IO;
+	using System.Threading.Tasks;
+
+	class Program
+	{
+		static void Main() => F().Wait();
+
+		static async Task F()
+		{
+			await new AsyncService().SomethingAsync();
+		}
+	}
+}
